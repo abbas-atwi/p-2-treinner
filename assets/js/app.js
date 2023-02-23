f02d5c2d292622aaaa68a1d5aea42eef2742178f
Abbas Atwi <abbas_9589_new@hotmail.com>
2023-02-23 20:53:21 -0300

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,26 @@
 let openModal = document.querySelector(".modal-container");
 let menu = document.querySelector(".menu");
 let caracterX = document.querySelector(".closeModal");
+let modalInput = (document.querySelector(".form-contact input").value = "");
+let enviar = document.querySelector(".enviar");
+enviar.addEventListener("click", (e) => {
+  e.preventDefault();
+});
 menu.addEventListener("click", () => {
   if (openModal.style.display == "none") {
     openModal.style.display = "flex";
+    modalInput.value = "";
   } else {
     openModal.style.display = "none";
+    modalInput.value = "";
   }
 });
 caracterX.addEventListener("click", () => {
   if (openModal.style.display == "none") {
     openModal.style.display = "flex";
+    modalInput.value = "";
   } else {
     openModal.style.display = "none";
+    modalInput.value = "";
   }
 });
